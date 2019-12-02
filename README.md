ede19584cf361aae9d33ddd0663b5c07f569d323
“Aishik <aishik.me@gmail.com>
2019-12-02 22:17:56 +0530

    Parse JSON body in HTTP request

---
+++ README.md
@@ -0,0 +1,3 @@
+# A simple URL shortener using Go and Angular
+
+This is the implementation of a simple URL shortener used by the author for a Golang talk at DBS Bank. No database is used and data is persisted on text files. Only native Go libraries are used. The frontend was built using Angular.
--- server.go
+++ server.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
+type myData struct {
+	Owner string
+	Name  string
+}
+
 func main() {
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	// http.HandleFunc("/api/v1/shorten", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	// })
+
+	http.HandleFunc("/api/v1/shorten", handleAddNewRepo)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleAddNewRepo(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+
+	var data myData
+	err := decoder.Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+
+	owner := data.Owner
+	// name := data.Name
+	fmt.Fprintf(w, "Hello, %q", owner)
 }
